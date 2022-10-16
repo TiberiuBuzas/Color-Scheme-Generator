1184ffb6ce2a4a5e07006747f3a3833fbaf9ec6f
Tiberiu Buzas <buzastibi310@gmail.com>
2022-10-16 15:04:37 +0200

    Small Changes

--- script.js
+++ script.js
@@ -1,24 +1,33 @@
 const button = document.getElementById("btn");
 const colorInput = document.getElementById("colorInput");
+const schemesInput = document.getElementById("schemes");
 const colorFenster = document.getElementsByClassName("colorFenster");
+const colors = document.getElementsByClassName("color");
+const hexH2 = document.getElementsByClassName("hexH2");
+const rgbH2 = document.getElementsByClassName("rgbH2");
+
+
+
 
 button.addEventListener("click", ()=>{
-    document.getElementsByClassName("colorPallet")[0].style.backgroundColor = colorInput.value;
+
+    fetch(`https://www.thecolorapi.com/scheme?hex=${colorInput.value.slice(1)}&mode=${schemesInput.value}&count=5`)
+        .then( (response) => response.json())
+        .then( (data) => {
+
+            for(let i = 0; i<colors.length; i++){
+                colors[i].style.backgroundColor = data.colors[i].hex.value;
+                colorFenster[i].style.backgroundColor = data.colors[i].hex.value;
+
+                hexH2[i].textContent = "HEX: " + data.colors[i].hex.value;
+                rgbH2[i].textContent = "RGB: " + data.colors[i].rgb.value.slice(3);
+
+            }
+
+        }); 
     
-    for(let fenster of colorFenster){
-        fenster.style.backgroundColor = colorInput.value;
-    }
 })
 
 
-fetch("https://www.thecolorapi.com", {
-    method: "PUT",
-    body: JSON.stringify({
-        color: colorInput.value
-    }),
-    headers: {
-        'Content-Type': 'application/json'
-    }
-})
-    .then( (response) => response.json())
-    .then( (data) => console.log(data));
+
+
